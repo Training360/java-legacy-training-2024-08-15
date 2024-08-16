04ed39e71e029f39669e436452876d1c15a21333
vicziani <viczian.istvan@gmail.com>
2024-08-16 15:50:53 +0200

    introducestaticsetter

--- dependency-breaking-problem/src/main/java/introducestaticsetter/EmployeeRepository.java
+++ dependency-breaking-problem/src/main/java/introducestaticsetter/EmployeeRepository.java
@@ -1,12 +1,24 @@
 package introducestaticsetter;
 
+import org.jetbrains.annotations.TestOnly;
+
 import java.util.List;
 
 public class EmployeeRepository {
 
+    private static EmployeeRepository instance;
 
     public static EmployeeRepository getInstance() {
+        if (instance != null) {
+            return instance;
+        }
         throw new IllegalStateException("Not initialized");
+    }
+
+    //@VisibleForTesting // privát lenne, de azért ,hogy a tesztből használhassukl, álllítjuk a láthatóságát
+    @TestOnly // ez a metódus CSAK tesztből hívható
+    public static void setInstance(EmployeeRepository instance) {
+        EmployeeRepository.instance = instance;
     }
 
     public List<String> findAll() {
